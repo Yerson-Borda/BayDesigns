45f36a73ede20ba27c7d7dc00c38ecd7f04d0902
Yerson-Borda <kira_yerson2407@hotmail.com>
2025-01-16 21:00:57 +0700

    Base implementation of AR
    Dependency (Sceneview) to work with AR and, two 3D models added in the asset package, and simple code for the model to show up on the camera

--- app/src/main/java/com/example/baydesigns/MainActivity.kt
+++ app/src/main/java/com/example/baydesigns/MainActivity.kt
@@ -1,29 +1,41 @@
 package com.example.baydesigns
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.google.ar.core.Config
 import com.example.baydesigns.ui.theme.BayDesignsTheme
+import io.github.sceneview.ar.ARScene
+import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.ar.node.ArNode
+import io.github.sceneview.ar.node.PlacementMode
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BayDesignsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    Box(modifier = Modifier.fillMaxSize()){
+                        val currentModel = remember {
+                            mutableStateOf("sofa")
+                        }
+                        ARScreen(currentModel.value)
+                    }
                 }
             }
         }
@@ -31,17 +43,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ARScreen(model:String) {
+    val nodes = remember {
+        mutableListOf<ArNode>()
+    }
+    val modelNode = remember {
+        mutableStateOf<ArModelNode?>(null)
+    }
+    val placeModelButton = remember {
+        mutableStateOf(false)
+    }
+    Box(modifier = Modifier.fillMaxSize()){
+        ARScene(
+            modifier = Modifier.fillMaxSize(),
+            nodes = nodes,
+            planeRenderer = true,
+            onCreate = {arSceneView ->
+                arSceneView.lightEstimationMode = Config.LightEstimationMode.DISABLED
+                arSceneView.planeRenderer.isShadowReceiver = false
+                modelNode.value = ArModelNode(arSceneView.engine,PlacementMode.INSTANT).apply {
+                    loadModelGlbAsync(
+                        glbFileLocation = "models/${model}.glb",
+                        scaleToUnits = 0.8f
+                    ){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BayDesignsTheme {
-        Greeting("Android")
+                    }
+                    onAnchorChanged = {
+                        placeModelButton.value = !isAnchored
+                    }
+                    onHitResult = {node, hitResult ->
+                        placeModelButton.value = node.isTracking
+                    }
+
+                }
+                nodes.add(modelNode.value!!)
+            },
+            onSessionCreate = {
+                planeRenderer.isVisible = false
+            }
+        )
+
+        // This is just for vizualization purposes, we can remove it later
+        if(placeModelButton.value){
+            Button(onClick = {
+                modelNode.value?.anchor()
+            }, modifier = Modifier.align(Alignment.Center)) {
+                Text(text = "Place It")
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = model){
+        modelNode.value?.loadModelGlbAsync(
+            glbFileLocation = "models/${model}.glb",
+            scaleToUnits = 0.8f
+        )
+        Log.e("errorloading","ERROR LOADING MODEL")
     }
 }
